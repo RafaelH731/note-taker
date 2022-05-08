486b76f394b2972e0cbf29b7f70c639e17d4caeb
Rafael Herrera <rafaelherrera731@gmail.com>
2022-05-07 20:55:21 -0700

    connected to api notes and static html

--- index.js
+++ index.js
@@ -6,3 +6,25 @@ const fs = require("fs");
 //port
 const app = express();
 const PORT = 3001;
+
+//app use functions
+app.use(express.json());
+//connects to the static files in the public folder
+app.use(express.static("public"));
+
+
+//routes
+ app.get("/api/notes", (req, res) => {
+     fs.readFile("db/db.json", "utf8", function (err, data){
+         if (err){
+             console.log(err);
+             return;
+         } res.json(notes)
+     })
+ });
+
+
+ //server 
+ app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
