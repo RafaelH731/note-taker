4c0a20f0319b7e8d5ec693cbfef8dca632076443
Rafael Herrera <rafaelherrera731@gmail.com>
2022-05-07 21:30:29 -0700

    connected html

--- index.js
+++ index.js
@@ -11,9 +11,15 @@ const PORT = 3001;
 app.use(express.json());
 //connects to the static files in the public folder
 app.use(express.static("public"));
-
+app.use(express.urlencoded({extended: true}));
+let notes = require("./db/db.json")
 
 //routes
+app.get("/notes", (req, res) =>{
+    res.sendfile(path.join(__dirname, "public/notes.html"));
+});
+
+//to display notes
  app.get("/api/notes", (req, res) => {
      fs.readFile("db/db.json", "utf8", function (err, data){
          if (err){
